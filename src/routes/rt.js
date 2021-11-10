0e7f02fd2bd14a84c4b232f0066161530790e857
Jose <jcur1802@gmail.com>
2021-11-10 01:56:53 -0600

    Logica del API completa

--- src/routes/rt.js
+++ src/routes/rt.js
@@ -47,11 +47,64 @@ router.post('/addCourse', (req, res)=> {
     } else {
         res.send('Data missing')
     }
-})
+});
 
 // PUT - UPDATE
-//router.put('/updateCourse', (req, res) => {
-    
-//})
+router.put('/updateCourse/:key', (req, res) => {
+    const { course, code, grade} = req.body;
+    const { key } = req.params;
+
+    if(course && code && grade){
+        try {
+            const jsonString = fs.readFileSync('src/db.json', 'utf-8');
+            const data = JSON.parse(jsonString);
+
+            data.forEach(element => {
+                if(element.code == key){
+                    element.course = course;
+                    element.grade = grade;
+                }
+            });
+            const jsonData = JSON.stringify(data, null, 2);
+            fs.writeFile('src/db.json', jsonData, err => {
+                if(err){
+                    console.log(err)
+                } else{
+                    console.log('Update Success')
+                    res.send('Update OK')
+                }
+            })
+        } catch(err){
+            console.log(err);
+        }
+    } else{
+        res.send('Attribute missing');
+    }
+});
+
+// DELETE
+router.delete('/deleteCourse/:key', (req, res) => {
+    const { key } = req.params;
+    try {
+        const jsonString = fs.readFileSync('src/db.json', 'utf-8');
+        const data = JSON.parse(jsonString);
+
+        for(var i = 0; i < data.length; i++){
+            if(data[i].code == key){
+                data.splice(i, 1);
+            }
+        }
+        const jsonData = JSON.stringify(data, null, 2);
+        fs.writeFile('src/db.json', jsonData, err => {
+            if(err){
+                console.log(err);
+            } else {
+                res.send('Delete OK');
+            }
+        });
+    } catch(err){
+        console.log(err);
+    }
+});
 
 module.exports = router;
